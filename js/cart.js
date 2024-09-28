23788126096434aac746734334fe011b549ca184
mike <mikeqdev@users.noreply.github.com>
2024-09-28 13:59:29 -0400

    add cart and checkout

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+const cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+function addToCart(entry) {
+  // entry schema: name, size, price
+  const sku = `${entry.name}-${entry.size}`
+  if (!cart[sku]) {
+    cart[sku] = { quantity: 0 };
+  }
+  cart[sku].price = entry.price;
+  cart[sku].name = entry.name;
+  cart[sku].size = entry.size;
+  cart[sku].quantity++;
+  saveCart();
+}
+
+function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
