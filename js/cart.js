5e19039c469fab57c792e3cf310c2594665439a0
mike <mikeqdev@users.noreply.github.com>
2024-09-29 17:01:48 -0400

    add cart quantity count

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,10 @@
 const cart = JSON.parse(localStorage.getItem('cart')) || {};
 
+window.addEventListener('DOMContentLoaded', function() {
+   // wait to update cart otherwise cart-count element is not found
+  setTotalCartQuantity(); 
+});
+
 function addToCart(entry) {
   // entry schema: name, size, price
   const sku = `${entry.name}-${entry.size}`
@@ -18,4 +23,10 @@ function addToCart(entry) {
 
 function saveCart() {
   localStorage.setItem('cart', JSON.stringify(cart));
+  setTotalCartQuantity();
+}
+
+function setTotalCartQuantity(){
+  const val = Object.values(cart).reduce((sum, item) => sum + item.quantity, 0);
+  document.getElementById('cart-count').innerText = val > 0 ? val : '';
 }
